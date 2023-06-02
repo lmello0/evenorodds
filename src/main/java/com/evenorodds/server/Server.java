d8776367660a217e93efd60e88b14c8789bea2c7
Lucas M <melloluc02@gmail.com>
2023-06-01 23:34:41 -0300

    Import fix

--- src/main/java/com/evenorodds/server/Server.java
+++ src/main/java/com/evenorodds/server/Server.java
@@ -1,8 +1,6 @@
 package com.evenorodds.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.Scanner;
